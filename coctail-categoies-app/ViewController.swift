1c3de37cbde259100698437a3481cf56fcb75ce1
Serhiy Prikhodko <serhii.prikhodko@gmail.com>
2020-03-25 20:22:01 +0200

    Initial Commit

---
+++ coctail-categoies-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coctail-categoies-app
+//
+//  Created by Serhiy Prikhodko on 3/25/20.
+//  Copyright © 2020 Serhiy Prikhodko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
